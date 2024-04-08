391bb4a58f802bd12bbab9f1b9a2a9c582e15d12
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-08 17:48:18 +0200

    ex0: do the same with more cmde

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # initializes a new C# project inside a folder titled 0-new_project.
-dotnet new console -o 0-new_project
+mkdir 0-new_project
+cd 0-new_project
+dotnet new console
