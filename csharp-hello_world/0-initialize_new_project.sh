41f442bcf35a22fe310e6644bc7062d8edabd3cd
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-09 12:48:50 +0200

    ex0 again, same code as my peer ;), check4 should pass

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 # initializes a new C# project inside a folder titled 0-new_project.
-project="0-new_project"
-mkdir "$project"
-
-cd "$project"
-
+mkdir 0-new_project
+cd 0-new_project
 dotnet new console
