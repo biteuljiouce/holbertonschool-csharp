e8556b06ce7a96237a8b3d1dcae133051e245ec4
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-09 13:48:04 +0200

    ex9

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -10,4 +10,3 @@ class Program
 		Console.WriteLine("Percent: {0:0.00%}\nCurrency: {1}", percent, currency.ToString("C", provider));
 	}
 }
-// 		Console.WriteLine("Percent: {0:0.00%}\nCurrency: {1}", percent, currency.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-US.utf-8")));
