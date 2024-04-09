082921a30d9458e0410e46a085bfcf1669a1dc00
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-09 13:45:12 +0200

    ex9

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        System.Globalization.CultureInfo provider = new System.Globalization.CultureInfo("en-US.utf-8");
+		Console.WriteLine("Percent: {0:0.00%}\nCurrency: {1}", percent, currency.ToString("C", provider));
+	}
+}
+// 		Console.WriteLine("Percent: {0:0.00%}\nCurrency: {1}", percent, currency.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-US.utf-8")));
