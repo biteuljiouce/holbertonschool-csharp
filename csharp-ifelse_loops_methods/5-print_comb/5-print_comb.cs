bde245833a0c0fd0bff9f8ea376b8f3089380693
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-10 12:04:30 +0200

    ex5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string end = ", ";
+            for (int i = 0; i <= 99; i++)
+            {
+                if (i == 99)
+                {
+                    end = "\n";
+                }
+                Console.Write($"{i:D2}{end}");
+            }
+        }
+    }
+}
