783d4514156db5b883168dc6be8b0fa1833e6405
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-10 12:51:14 +0200

    ex8

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0)
+            number = number * -1;
+        int lastDigit = number % 10;
+        Console.Write(lastDigit.ToString());
+        return (lastDigit);
+    }
+}
