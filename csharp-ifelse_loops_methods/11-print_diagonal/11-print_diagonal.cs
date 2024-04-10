3f7d32e54e93d724df3bf514377012b39ffe85db
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-10 13:22:55 +0200

    ex11

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+            for (int i = 0; i < length; i++)
+            {
+                for (int sp = 0; sp < i; sp++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("\\");
+                Console.WriteLine();
+            }
+        else
+            Console.WriteLine();
+    }
+}
