cd1573c655b9779bd8c4ecd3613ec93a598c823e
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-10 11:14:49 +0200

    ex1

---
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//  assign a random signed number to the variable number each time it is executed.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = number % 10;
+        string str = "";
+        if (number > 5)
+        {
+            str = "and is greater than 5";
+        }
+        else if (number < 6 && number != 0)
+        {
+            str = "and is less than 6 and not 0";
+        }
+        else if (number == 0)
+        {
+            str = "and is 0";
+        }
+        Console.WriteLine($"The last digit of {number} is {lastDigit} {str}");
+    }
+}
