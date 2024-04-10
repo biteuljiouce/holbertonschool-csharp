5022b495160f851d1b5a823311ea55719dcfc299
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-10 11:52:44 +0200

    ex3

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            foreach (char letter in alpha)
+            {
+                if (letter.Equals('q') || letter.Equals('e'))
+                {
+                    continue;
+                }
+                Console.Write(letter.ToString());
+            }
+        }
+    }
+}
