ad2bbb40d9b9863ec380b93eb7a18d0c2603b30a
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-10 13:36:13 +0200

    ex12

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "";
+        int max = 100;
+        for (int i = 1; i <= max; i++)
+        {
+            if (i % 3 == 0)
+            {
+                if (i % 5 == 0)
+                    str = "FizzBuzz";
+                str = "Fizz";
+            }
+            else if (i % 5 == 0)
+                str = "Buzz";
+            else
+                str = i.ToString();
+            Console.Write($"{str}");
+            if (i < max)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
