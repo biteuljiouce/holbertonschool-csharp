842f351f80e9b1d905c3ecb917c1c88d7f7b21e6
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-10 20:11:25 +0200

    ex1

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -36,7 +36,13 @@ class Array
     // retrieves an element from an array.
     public static int elementAt(int[] array, int index)
     {
-
+        if (index >= 0 && index < array.Length)
+            return array[index];
+        else
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
     }
 }
 
