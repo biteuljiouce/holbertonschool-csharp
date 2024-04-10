ee4f00f6be8aad8c271c473dae1d2180d7ce5dd2
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-10 11:40:56 +0200

    ex2

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            foreach (char letter in alpha)
+            {
+                Console.Write(letter.ToString());
+            }
+        }
+    }
+}
