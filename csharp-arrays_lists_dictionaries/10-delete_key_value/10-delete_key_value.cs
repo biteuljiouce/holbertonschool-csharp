ca37daba67e96727c4cd6a64d29111f482882514
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-11 17:32:06 +0200

    ex10

---
+++ csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    //  returns the number of keys in a dictionary.
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        return myDict.Count;
+    }
+
+    // adds a key and value to a dictionary.
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (myDict.ContainsKey(key))
+            myDict[key] = value;
+        else
+            myDict.Add(key, value);
+        return myDict;
+    }
+
+    // deletes a key in a dictionary.
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
+        myDict.Remove(key);
+        return myDict;
+    }
+}
