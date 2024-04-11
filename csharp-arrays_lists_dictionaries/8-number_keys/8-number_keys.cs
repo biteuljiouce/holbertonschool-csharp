ef5a52134be965a6edda086eb13af1905b211fc0
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-11 17:20:02 +0200

    ex8

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    //  returns the number of keys in a dictionary.
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        return myDict.Count;
+    }
+}
