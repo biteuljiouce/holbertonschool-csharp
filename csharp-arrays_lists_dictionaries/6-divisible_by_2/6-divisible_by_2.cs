bbbc039b8c24765bebd4c52548cc917c3aa28703
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-12 12:24:16 +0200

    ex6 : try to fix checker :/

--- csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -7,7 +7,7 @@ class List
     // finds all multiples of 2 in a list.
     public static List<bool> DivisibleBy2(List<int> myList)
     {
-        List<bool> isMultipleList = new List<bool>();
+        List<bool> isMultipleList = new List<bool>(myList.Count);
         // finds all multiples of 2 in a list.
         myList.ForEach(n => { Console.Write(n); isMultipleList.Add((n % 2 == 0) ? true : false); });
         // return it
