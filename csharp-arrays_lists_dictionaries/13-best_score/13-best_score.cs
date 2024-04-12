86a776098cbc193afd195b41589f0efff0266702
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-12 09:48:37 +0200

    ex13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    // returns the key with the biggest integer value in a given dictionary.
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = -1;
+        foreach (KeyValuePair<string, int> item in myList)
+        {
+            if (item.Value > max)
+            {
+                max = item.Value;
+            }
+        }
+        return (max == -1) ? "None" : $"{max}";
+    }
+
+    // prints a dictionary by ordered keys.
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> sortedKeys = new List<string>();
+        foreach (var item in myDict)
+            sortedKeys.Add(item.Key);
+        sortedKeys.Sort();
+        sortedKeys.ForEach((string str) => Console.WriteLine(str));
+    }
+}
