cc9a3e3f8bd82b3dcede77db984c82c9a3961ea5
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-12 11:44:12 +0200

    ex15

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Matrix
+{
+    // computes the square value of all integers of a matrix.
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int[,] squares = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
+        squares = myMatrix;
+        for (int x = 0; x < myMatrix.GetLength(0); x++)
+        {
+            for (int y = 0; y < myMatrix.GetLength(1); y++)
+            {
+                squares[x, y] = squares[x, y] * squares[x, y];
+            }
+        }
+        return squares;
+    }
+}
