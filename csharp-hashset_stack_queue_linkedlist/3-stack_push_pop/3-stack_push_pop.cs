09e751f4c5a4c493419cf0b7da2edaf0c95d4f18
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 11:51:12 +0200

    ex3

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    /*
+    does the following:
+    Print the number of items in aStack
+        Format: Number of items: <number>
+    Print the item at the top of aStack without removing it
+        Format: Top item: <item>
+        If aStack is empty, print Stack is empty
+    Print if aStack contains a given item search
+        Format: Stack contains <search>: <True / False>
+    If aStack contains the given item search, remove all items up to and including search; otherwise, leave aStack as is
+        You can use .Pop() only once
+    Add a new given item newItem to aStack
+    Return aStack
+    */
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int count = aStack.Count;
+        Console.WriteLine($"Number of items: {count}");
+        if (count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        bool containsSearch = aStack.Contains(search);
+        if (containsSearch)
+        {
+            string item;
+            do
+            {
+                item = aStack.Pop();
+            }
+            while (item != search);
+        }
+        Console.WriteLine($"Stack contains \"{search}\": {containsSearch}");
+        if (newItem != null)
+            aStack.Push(newItem);
+        return aStack;
+    }
+}
