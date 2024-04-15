3b6c7e8042c7dc3efc8ab87ccf05275345265094
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 12:47:28 +0200

    ex8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    //  finds a value in a LinkedList and returns its index position in the list.
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+        LinkedListNode<int> node = myLList.First;
+        while (node != null)
+        {
+            if (node.Value == value)
+            {
+                return index;
+            }
+            index++;
+            node = node.Next;
+        }
+        // value not found
+        return -1;
+    }
+}
