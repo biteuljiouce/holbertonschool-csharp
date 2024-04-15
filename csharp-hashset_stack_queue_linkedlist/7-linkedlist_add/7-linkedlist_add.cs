8cbdfc3ae01064d13993a1c13bea3d7861bc577e
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 12:29:04 +0200

    ex7

---
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // adds a node to the beginning of a LinkedList.
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> node = new LinkedListNode<int>(n);
+        myLList.AddFirst(node);
+        return node;
+    }
+}
