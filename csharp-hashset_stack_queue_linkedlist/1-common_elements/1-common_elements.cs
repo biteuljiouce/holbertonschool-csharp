c76aae1b1889b76eeecc068566045913668ec861
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 11:00:54 +0200

    ex1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    //  returns a sorted list of common elements in two lists.
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> sortedCommons = new List<int>();
+        foreach (int i in list1)
+        {
+            if (list2.Contains(i))
+            {
+                sortedCommons.Add(i);
+            }
+        }
+        sortedCommons.Sort();
+        return sortedCommons;
+    }
+}
