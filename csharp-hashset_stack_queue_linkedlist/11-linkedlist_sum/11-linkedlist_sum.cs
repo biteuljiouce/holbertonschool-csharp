5cbfead87b74beb1d9b1ced494a97bf4317f83f3
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 13:04:09 +0200

    ex11

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // returns the sum of all the data of a LinkedList.
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        LinkedListNode<int> node = myLList.First;
+        while (node != null)
+        {
+            sum += node.Value;
+            node = node.Next;
+        }
+        return sum;
+    }
+}
