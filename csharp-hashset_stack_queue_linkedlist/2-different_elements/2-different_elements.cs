1051f276a4fc1e3dd3acf4efaeec09d05b1aeea2
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 11:08:12 +0200

    ex2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // returns a sorted list of all elements present in one or the other list but not both.    
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> sortedDiffs = new List<int>();
+        foreach (int i in list1)
+        {
+            if (!list2.Contains(i))
+            {
+                sortedDiffs.Add(i);
+            }
+        }
+        foreach (int i in list2)
+        {
+            if (!list1.Contains(i))
+            {
+                sortedDiffs.Add(i);
+            }
+        }
+        sortedDiffs.Sort();
+        return sortedDiffs;
+    }
+}
