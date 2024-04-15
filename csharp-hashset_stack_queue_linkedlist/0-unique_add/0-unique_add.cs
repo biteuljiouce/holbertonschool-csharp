dee72a4a5c775d6ce884e99bb4c5e41e1a838bf6
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 10:30:57 +0200

    ex0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    //  Returns the sum of unique integers in given list.
+    public static int Sum(List<int> myList)
+    {
+        //myList.ForEach(item => Console.WriteLine(item));
+        //Console.WriteLine();
+        List<int> noDouble = new List<int>();
+        int sum = myList[0];
+        noDouble.Add(myList[0]);
+        foreach (int nb in myList)
+        {
+            if (!noDouble.Contains(nb))
+            {
+                noDouble.Add(nb);
+                sum += nb;
+            }
+        }
+        //noDouble.ForEach(item => Console.WriteLine(item));
+        return sum;
+    }
+}
