82cbc441c31b0cefadd72c46811db8b2b4130529
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 12:15:25 +0200

    ex5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // creates and prints a LinkedList of integers of a given size.
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            return new LinkedList<int>();
+        }
+        LinkedList<int> llist = new LinkedList<int>();
+        for (int i = 0; i < size; i++)
+        {
+            llist.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return llist;
+    }
+}
