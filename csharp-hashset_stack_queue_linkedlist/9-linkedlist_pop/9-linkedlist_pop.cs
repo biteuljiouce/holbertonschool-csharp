90559880850d9ad462d568c0812783bd01164dcb
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 12:53:55 +0200

    ex9

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // deletes the head node of a LinkedList and returns that node’s data.
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int value = 0;
+        if (myLList.Count > 0)
+        {
+            value = myLList.First.Value;
+            myLList.RemoveFirst();
+        }
+        return value;
+    }
+}
