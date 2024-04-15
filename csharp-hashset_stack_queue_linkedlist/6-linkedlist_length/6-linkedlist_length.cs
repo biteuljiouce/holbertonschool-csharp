3a09553a333ab5e1f0f75045260727276a4e6c14
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 12:19:01 +0200

    ex6

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // returns the number of elements in a LinkedList.
+    public static int Length(LinkedList<int> myLList)
+    {
+        return myLList.Count;
+    }
+}
