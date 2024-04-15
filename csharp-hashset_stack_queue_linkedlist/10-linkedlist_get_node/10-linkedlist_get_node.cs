72f2e41f3f5451625c182921519cdc9448a55923
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-15 13:01:25 +0200

    ex10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    //  returns the value of the nth node of a LinkedList.
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+        LinkedListNode<int> node = myLList.First;
+        while (node != null)
+        {
+            if (index == n)
+            {
+                return node.Value;
+            }
+            index++;
+            node = node.Next;
+        }
+        return index;
+    }
+}
