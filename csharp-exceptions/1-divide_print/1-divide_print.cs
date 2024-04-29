7658900ea037726abf14a41d2eaa54a5d203e4a1
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-04-29 15:53:06 +0200

    exo1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Int
+{
+    // divides 2 integers and prints the result.
+    public static void divide(int a, int b)
+    {
+        float result = 0;
+        try
+        {
+            result = a / b;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        catch (Exception ex)
+        {
+
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+    }
+}
+
