88f99c811e78f80a32bf919a17cbc1dc002f72e6
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-05-06 19:32:56 +0200

    ex1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class represents a zombie.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>
+        /// health of a zombie. dead when equals zero.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// constructor with no param.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
