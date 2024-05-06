e80389a9e163248f555665c4a0b185b92e151657
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-05-06 19:47:05 +0200

    ex3

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class represents a zombie.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>
+        /// health of a zombie. dead when equals zero.
+        /// </summary>
+        private int _health;
+
+        /// <summary>
+        /// create a zombie.
+        /// </summary>
+        public Zombie()
+        {
+            _health = 0;
+        }
+
+        /// <summary>
+        /// create a zombie.
+        /// </summary>
+        /// <param name="health">
+        /// health of the zombie (&gt;= 0)
+        /// </param>
+        public Zombie(int health)
+        {
+            if (health < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this._health = health;
+        }
+
+        /// <summary>
+        /// Get the health value.
+        /// </summary>
+        /// <returns>health value (integer &gt;= 0)</returns>
+        public int GetHealth()
+        {
+            return _health;
+        }
+    }
+}
