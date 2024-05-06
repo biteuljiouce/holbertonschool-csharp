5e5f132ef8b343368110862c582787cb5a2e7901
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-05-06 21:30:11 +0200

    ex5

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class represents a zombie.
+    /// </summary>
+    class Zombie
+    {
+        // health of a zombie. dead when equals zero.
+        private int health;
+
+        // name of a zombie.
+        private string name = "(No name)";
+
+        /// <summary>
+        /// name of a zombie.
+        /// </summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        /// <summary>
+        /// create a zombie.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// create a zombie with given health.
+        /// </summary>
+        /// <param name="health">
+        /// health of the zombie (&gt;= 0)
+        /// </param>
+        public Zombie(int health)
+        {
+            if (health < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = health;
+
+        }
+
+        /// <summary>
+        /// Get the health value.
+        /// </summary>
+        /// <returns>health value (integer &gt;= 0)</returns>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+        /// <summary>
+        /// Prints info about zombie.
+        /// </summary>
+        /// <returns>description as string</returns>
+        public override string ToString() => $"Zombie Name: {name} / Total Health: {health}";
+    }
+}
