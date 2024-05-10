41871eb5648e4a8209cec4c55d6518fca19c63e9
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-05-10 23:32:09 +0200

    ex1 add tests

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -40,6 +40,50 @@ namespace MyMath.Tests
         /// test
         /// </summary>
         [Test]
+        public void Divide_by_n1()
+        {
+            // Arrange
+            int[,] a = {
+               { 1 , 2 , 3 , } ,
+               { 4 , 5 , 6 , } ,
+             };
+            int b = -1;
+            int[,] good = {
+                { -1 , -2 , -3 , } ,
+                { -4 , -5 , -6 ,} ,
+             };
+            // Act
+            int[,] result = Operations.Divide(a, b);
+            // Assert
+            Assert.AreEqual(good, result);
+        }
+
+        /// <summary>
+        /// test
+        /// </summary>
+        [Test]
+        public void Divide_by_100()
+        {
+            // Arrange
+            int[,] a = {
+               { 1 , 2 , 3 , } ,
+               { 4 , 5 , 6 , } ,
+             };
+            int b = 100;
+            int[,] good = {
+                { 0 , 0 , 0 , } ,
+                { 0 , 0 , 0 ,} ,
+             };
+            // Act
+            int[,] result = Operations.Divide(a, b);
+            // Assert
+            Assert.AreEqual(good, result);
+        }
+
+        /// <summary>
+        /// test
+        /// </summary>
+        [Test]
         public void Divide_by_2()
         {
             // Arrange
