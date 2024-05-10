aafd28c58ef2973b0aa16204be8d1841284a0f04
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-05-10 18:34:28 +0200

    ex1

---
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,93 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+
+    /// <summary>
+    ///  The [TestFixture] attribute denotes a class that contains unit tests.
+    /// </summary> 
+    /// <see cref="https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-nunit"/>>
+    [TestFixture]
+    public class OperationsTests
+    {
+        /// <summary>
+        /// does nothing.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// test
+        /// </summary>
+        [Test]
+        public void Divide_by_1()
+        {
+            // Arrange
+            int[,] a = {
+               { 1 , 2 , 3 , } ,
+               { 4 , 5 , 6 , } ,
+             };
+            int b = 1;
+            // Act
+            int[,] result = Operations.Divide(a, b);
+            // Assert
+            Assert.AreEqual(a, result);
+        }
+
+        /// <summary>
+        /// test
+        /// </summary>
+        [Test]
+        public void Divide_by_2()
+        {
+            // Arrange
+            int[,] a = {
+               { 2 , 4 , 6 , } ,
+               { 8 , 10 , 12 , } ,
+             };
+            int b = 2;
+            int[,] good = {
+                { 1 , 2 , 3 , } ,
+                { 4 , 5 , 6 ,} ,
+             };
+            // Act
+            int[,] result = Operations.Divide(a, b);
+            // Assert
+            Assert.AreEqual(good, result);
+        }
+        /// <summary>
+        /// test
+        /// </summary>
+        [Test]
+        public void Divide_by_0_returns_null()
+        {
+            // Arrange
+            int[,] a = {
+               { 2 , 4 , 6 , } ,
+               { 8 , 10 , 12 , } ,
+             };
+            int b = 0;
+            // Act
+            int[,] result = Operations.Divide(a, b);
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+
+        /// <summary>
+        /// test
+        /// </summary>
+        [Test]
+        public void Matrix_is_null_returns_null()
+        {
+            // Arrange
+            int[,] a = null;
+            int b = 2;
+            // Act
+            int[,] result = Operations.Divide(a, b);
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+    }
+}
---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyMath
+{
+
+    /// <summary>
+    /// Math operations
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Add 2 numbers
+        /// </summary>
+        /// <returns>The sum of two integers</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Divides a matrix by an int.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="num"></param>
+        /// <returns>divided matrix</returns>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+                return null;
+            int row = matrix.GetLength(0);
+            int col = matrix.GetLength(1);
+            int[,] result = new int[row, col];
+            try
+            {
+                for (int i = 0; i < row; i++)
+                    for (int j = 0; j < col; j++)
+                    {
+                        result[i, j] = matrix[i, j] / num;
+                    }
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            return result;
+        }
+    }
+}
