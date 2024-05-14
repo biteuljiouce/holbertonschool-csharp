c5f035c009f145430db4db2a96510e62eb1c1e02
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-05-14 17:11:36 +0200

    ex9

---
+++ csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+/// <summary>
+/// represents a geometric shape.
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// calculates area.
+    /// </summary>
+    /// <returns>area as integer</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// represents a rectangle.
+/// </summary>
+class Rectangle : Shape
+{
+    // width of rectangle
+    private int width;
+    // height of rectangle
+    private int height;
+
+    /// <summary>
+    /// Width of rectangle 
+    /// </summary>
+    /// <value></value>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    /// <summary>
+    /// Width of rectangle 
+    /// </summary>
+    /// <value></value>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// returns the area of the rectangle
+    /// </summary>
+    /// <returns>area as int</returns>
+    public new int Area()
+    {
+        return width * height;
+    }
+
+    /// <summary>
+    /// description as string.
+    /// </summary>
+    /// <returns>string</returns>
+    public override string ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
+    }
+}
+
+
+/// <summary>
+/// represents a square.
+/// </summary>
+class Square : Rectangle
+{
+    // size of square
+    private int size;
+
+    /// <summary>
+    /// Size of square 
+    /// </summary>
+    /// <value></value>
+    public int Size
+    {
+        get { return size; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+            else
+            {
+                Width = Height = size = value;
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return $"[Square] {Width} / {Height}";
+    }
+}
