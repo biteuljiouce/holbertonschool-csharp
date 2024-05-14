9919f721b349c92d44b5608c828c66ab3d6d82ec
Cedric H <604281+biteuljiouce@users.noreply.github.com>
2024-05-14 16:16:28 +0200

    ex4

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// represents a dog.
+/// </summary>
+class Dog : Animal
+{
+    // Empty class
+}
+
+/// <summary>
+/// represents an animal.
+/// </summary>
+class Cat : Animal
+{
+    // Empty class
+}
